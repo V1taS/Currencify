92560c4f1a78d35314909c98f02f05de6f13c999
V1taS <SosinVitalii@iCloud.com>
2024-09-29 16:00:16 +0200

    Refactoring

--- Currencify/App/Sources/Modules/MainScreenModule/Presenter/MainScreenPresenter.swift
+++ Currencify/App/Sources/Modules/MainScreenModule/Presenter/MainScreenPresenter.swift
@@ -49,6 +49,7 @@ final class MainScreenPresenter: ObservableObject {
   
   lazy var viewDidLoad: (() -> Void)? = {}
   
+  /// Метод, вызываемый перед появлением представления; проверяет режим Premium.
   lazy var viewWillAppear: (() -> Void)? = { [weak self] in
     Task { [weak self] in
       guard let self else { return }
@@ -58,11 +59,14 @@ final class MainScreenPresenter: ObservableObject {
   
   // MARK: - Internal func
   
+  /// Обновляет данные о курсах валют и создает виджеты валют.
   func refreshCurrencyData() async {
     await interactor.fetchCurrencyRates()
     await createCurrencyWidget()
   }
   
+  /// Добавляет новую валюту в список выбранных пользователем.
+  /// - Parameter newValue: Новая валюта для добавления.
   func userAddCurrencyRate(newValue: CurrencyRate) async {
     let appSettingsModel = await getAppSettingsModel()
     isSearchViewVisible = false
@@ -79,6 +83,8 @@ final class MainScreenPresenter: ObservableObject {
     await createCurrencyWidget()
   }
   
+  /// Удаляет валюту из списка выбранных пользователем.
+  /// - Parameter currencyAlpha: Код валюты для удаления.
   func userRemoveCurrencyRate(currencyAlpha: String) async {
     let appSettingsModel = await getAppSettingsModel()
     guard let currency = CurrencyRate.Currency(rawValue: currencyAlpha) else {
@@ -95,6 +101,10 @@ final class MainScreenPresenter: ObservableObject {
     await createCurrencyWidget()
   }
   
+  /// Перемещает валюты в списке по заданным индексам.
+  /// - Parameters:
+  ///   - source: Индексы исходных элементов.
+  ///   - destination: Индекс назначения.
   @MainActor
   func moveCurrencyRates(from source: IndexSet, to destination: Int) async {
     let appSettingsModel = await getAppSettingsModel()
@@ -109,39 +119,40 @@ final class MainScreenPresenter: ObservableObject {
     DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
       Task { [weak self] in
         guard let self else { return }
-        await createCurrencyWidget()
+        await self.createCurrencyWidget()
       }
     }
   }
   
+  /// Пересчитывает значения виджетов валют.
   @MainActor
   func recalculateCurrencyWidgets() async {
     let appSettingsModel = await getAppSettingsModel()
     
-    let currencyRateIdentifiable = await interactor.recalculateCurrencyRates(
+    currencyRateIdentifiable = await interactor.recalculateCurrencyRates(
       appSettingsModel: appSettingsModel,
       rawCurrencyRate: rawCurrencyRate
     )
-    self.currencyRateIdentifiable = currencyRateIdentifiable
     await validateRatesData()
     await setCurrencyRateText()
   }
   
+  /// Создает виджеты валют на основе текущих данных.
   @MainActor
   func createCurrencyWidget() async {
     let appSettingsModel = await getAppSettingsModel()
     
     rawCurrencyRate = await getEnteredCurrencyAmountRaw(appSettingsModel)
     await recalculateCurrencyWidgets()
-    let models = factory.createCurrencyWidgetModels(
+    currencyWidgets = factory.createCurrencyWidgetModels(
       decimalPlaces: appSettingsModel.currencyDecimalPlaces,
       currencyRateIdentifiables: currencyRateIdentifiable
     )
-    currencyWidgets = models
     await setCurrencyRateText()
     await setKeyboardIsShown(appSettingsModel.isUserInputVisible, appSettingsModel.activeCurrency)
   }
   
+  /// Получает текущую модель настроек приложения.
   @MainActor
   func getAppSettingsModel() async -> AppSettingsModel {
     let appSettingsModel = await interactor.getAppSettingsModel()
@@ -149,26 +160,23 @@ final class MainScreenPresenter: ObservableObject {
     return appSettingsModel
   }
   
+  /// Устанавливает видимость пользовательского ввода.
+  /// - Parameter value: Логическое значение видимости.
   func setUserInputIsVisible(_ value: Bool) async {
     await interactor.setUserInputIsVisible(value)
   }
   
+  /// Устанавливает состояние клавиатуры для заданной валюты.
+  /// - Parameters:
+  ///   - value: Логическое значение отображения клавиатуры.
+  ///   - currency: Выбранная валюта.
   func setKeyboardIsShown(_ value: Bool, _ currency: CurrencyRate.Currency) async {
     await interactor.setUserInputIsVisible(value)
+    resetKeyboardShownState()
     
-    currencyWidgets.forEach { model in
-      let keyboardModelUpdate = model.keyboardModel
-      keyboardModelUpdate?.isKeyboardShown = false
-      model.keyboardModel = keyboardModelUpdate
-    }
-    
-    if let currencyWidgetsModel = currencyWidgets.first(where: { $0.additionalID == currency.rawValue }),
-       let currencyRateIdentifiableModel = currencyRateIdentifiable.first(where: { $0.currency.rawValue == currency.rawValue }) {
-      let keyboardModelUpdate = currencyWidgetsModel.keyboardModel
-      keyboardModelUpdate?.isKeyboardShown = value
-      keyboardModelUpdate?.value = currencyRateIdentifiableModel.rateText
+    if let (currencyWidgetsModel, currencyRateIdentifiableModel) = getCurrencyModels(for: currency) {
+      updateKeyboardModel(for: currencyWidgetsModel, with: currencyRateIdentifiableModel, isShown: value)
       rawCurrencyRate = currencyRateIdentifiableModel.rateText
-      currencyWidgetsModel.keyboardModel = keyboardModelUpdate
     }
   }
 }
@@ -183,20 +191,25 @@ extension MainScreenPresenter: MainScreenInteractorOutput {}
 
 // MARK: - MainScreenFactoryOutput
 
-extension MainScreenPresenter: MainScreenFactoryOutput {  
+extension MainScreenPresenter: MainScreenFactoryOutput {
+  
+  /// Обрабатывает ввод суммы пользователем.
+  /// - Parameter newValue: Новое введенное значение.
   func userDidEnterAmount(_ newValue: String) async {
-    let newValue = modifyAmount(newValue)
+    let modifiedValue = modifyAmount(newValue)
     let appSettingsModel = await getAppSettingsModel()
     
-    await setRawCurrencyRate(newValue: newValue)
+    await setRawCurrencyRate(newValue: modifiedValue)
     
-    let countCharactersAfterComma = interactor.textFormatterService.countCharactersAfterComma(in: newValue)
-    if let countCharactersAfterComma, countCharactersAfterComma > appSettingsModel.currencyDecimalPlaces.rawValue {
+    if let countCharactersAfterComma = interactor.textFormatterService.countCharactersAfterComma(in: modifiedValue),
+       countCharactersAfterComma > appSettingsModel.currencyDecimalPlaces.rawValue {
       triggerHapticFeedback(.error)
     }
     await recalculateCurrencyWidgets()
   }
   
+  /// Обрабатывает выбор валюты пользователем.
+  /// - Parameter currency: Выбранная валюта.
   func userDidSelectCurrency(_ currency: CurrencyRate.Currency) async {
     let appSettingsModel = await getAppSettingsModel()
     
@@ -208,11 +221,7 @@ extension MainScreenPresenter: MainScreenFactoryOutput {
     }
     
     await interactor.setActiveCurrency(currency)
-    if let currencyRateIdentifiableModel = currencyRateIdentifiable.first(
-        where: {
-          $0.currency.rawValue == currency.rawValue
-        }
-       ) {
+    if let (_, currencyRateIdentifiableModel) = getCurrencyModels(for: currency) {
       rawCurrencyRate = currencyRateIdentifiableModel.rateText
     }
     
@@ -250,9 +259,9 @@ extension MainScreenPresenter: SceneViewModel {
         .share(
           action: { [weak self] in
             Task { [weak self] in
-              guard let self else { return}
+              guard let self else { return }
               if await interactor.requestGallery() {
-                await createCollectionViewSnapshot()
+                await self.createCollectionViewSnapshot()
               }
             }
           }, buttonItem: { [weak self] buttonItem in
@@ -278,6 +287,8 @@ extension MainScreenPresenter: SceneViewModel {
 // MARK: - Private
 
 private extension MainScreenPresenter {
+  
+  /// Проверяет данные о курсах валют и обновляет состояние видимости элементов.
   @MainActor
   func validateRatesData() async {
     let appSettingsModel = await getAppSettingsModel()
@@ -286,84 +297,137 @@ private extension MainScreenPresenter {
     rightBarShareButton?.isEnabled = !appSettingsModel.allCurrencyRate.isEmpty
   }
   
+  /// Создает снимок коллекции виджетов и открывает его в просмотрщике изображений.
   func createCollectionViewSnapshot() async {
     let finalImage = await interactor.createCollectionViewSnapshot()
     await moduleOutput?.openImageViewer(image: finalImage)
   }
   
+  /// Инициирует тактильную обратную связь указанного типа.
+  /// - Parameter type: Тип обратной связи.
   func triggerHapticFeedback(_ type: UINotificationFeedbackGenerator.FeedbackType) {
     let generator = UINotificationFeedbackGenerator()
     generator.notificationOccurred(type)
   }
   
+  /// Устанавливает текстовые значения курсов валют в виджетах.
+  @MainActor
   func setCurrencyRateText() async {
     let appSettingsModel = await getAppSettingsModel()
     
-    currencyWidgets.forEach { currencyWidgetsModel in
+    for currencyWidgetsModel in currencyWidgets {
       if let currencyRateIdentifiableModel = currencyRateIdentifiable.first(
         where: {
           $0.currency.rawValue == currencyWidgetsModel.additionalID
-        }
-      ) {
-        let rightSideLargeTextModel = currencyWidgetsModel.rightSideLargeTextModel
-        rightSideLargeTextModel?.text = currencyRateIdentifiableModel.rateText
-        rightSideLargeTextModel?.textStyle = currencyRateIdentifiableModel.rateDouble == .zero ? .netural : .standart
-        currencyWidgetsModel.rightSideLargeTextModel = rightSideLargeTextModel
-        
-        let keyboardModelUpdate = currencyWidgetsModel.keyboardModel
-        keyboardModelUpdate?.value = currencyRateIdentifiableModel.rateText
-        currencyWidgetsModel.keyboardModel = keyboardModelUpdate
+        }) {
+        updateCurrencyWidgetsModel(currencyWidgetsModel, with: currencyRateIdentifiableModel)
       }
     }
     
-    if let currencyWidgetsModel = currencyWidgets.first(
-      where: {
-        $0.additionalID == appSettingsModel.activeCurrency.rawValue
+    if let (currencyWidgetsModel, currencyRateIdentifiableModel) = getCurrencyModels(for: appSettingsModel.activeCurrency) {
+      if let keyboardModelUpdate = currencyWidgetsModel.keyboardModel {
+        let countCharactersAfterComma = interactor.textFormatterService.countCharactersAfterComma(
+          in: currencyRateIdentifiableModel.rateText
+        ) ?? .zero
+        keyboardModelUpdate.keyboardIsBlock = countCharactersAfterComma >= appSettingsModel.currencyDecimalPlaces.rawValue
+        currencyWidgetsModel.keyboardModel = keyboardModelUpdate
       }
-    ),
-       let currencyRateIdentifiableModel = currencyRateIdentifiable.first(
-        where: {
-          $0.currency.rawValue == appSettingsModel.activeCurrency.rawValue
-        }
-       ) {
-      let keyboardModelUpdate = currencyWidgetsModel.keyboardModel
-      let countCharactersAfterComma = interactor.textFormatterService.countCharactersAfterComma(
-        in: currencyRateIdentifiableModel.rateText
-      ) ?? .zero
-      keyboardModelUpdate?.keyboardIsBlock = countCharactersAfterComma >= appSettingsModel.currencyDecimalPlaces.rawValue
+    }
+  }
+  
+  /// Модифицирует введенную сумму, приводя ее к корректному формату.
+  /// - Parameter newValue: Введенное значение.
+  /// - Returns: Модифицированное значение.
+  func modifyAmount(_ newValue: String) -> String {
+    let onlyCommaValue = newValue == "," ? "0," : newValue
+    return onlyCommaValue.isEmpty ? "0" : onlyCommaValue
+  }
+  
+  /// Устанавливает необработанное значение курса валюты.
+  /// - Parameter newValue: Новое значение.
+  func setRawCurrencyRate(newValue: String) async {
+    let newValueProcessed = interactor.textFormatterService.removeLeadingZeroIfNoComma(
+      in: interactor.textFormatterService.replaceDoubleZeroWithZero(in: newValue)
+    )
+    
+    let rawClearedTextCurrencyRate = interactor.textFormatterService.removeAllSpaces(from: newValueProcessed)
+    rawCurrencyRate = interactor.textFormatterService.formatNumberWithThousands(rawClearedTextCurrencyRate)
+    await saveEnteredCurrencyAmountRaw()
+  }
+  
+  /// Сохраняет введенную пользователем сумму в необработанном виде.
+  func saveEnteredCurrencyAmountRaw() async {
+    let newValueProcessed = interactor.textFormatterService.replaceCommasWithDots(
+      in: interactor.textFormatterService.removeAllSpaces(from: rawCurrencyRate)
+    )
+    await interactor.setEnteredCurrencyAmountRaw(newValueProcessed)
+  }
+  
+  /// Получает введенную пользователем сумму из настроек приложения.
+  /// - Parameter appSettingsModel: Модель настроек приложения.
+  /// - Returns: Введенная сумма в виде строки.
+  func getEnteredCurrencyAmountRaw(_ appSettingsModel: AppSettingsModel) async -> String {
+    let enteredCurrencyAmountRaw = appSettingsModel.enteredCurrencyAmountRaw
+    let rawReplaceDotsWithCommas = interactor.textFormatterService.replaceDotsWithCommas(in: enteredCurrencyAmountRaw)
+    return interactor.textFormatterService.formatNumberWithThousands(rawReplaceDotsWithCommas)
+  }
+  
+  /// Сбрасывает состояние отображения клавиатуры для всех виджетов.
+  func resetKeyboardShownState() {
+    for model in currencyWidgets {
+      if let keyboardModelUpdate = model.keyboardModel {
+        keyboardModelUpdate.isKeyboardShown = false
+        model.keyboardModel = keyboardModelUpdate
+      }
+    }
+  }
+  
+  /// Получает модели виджета и идентифицируемого курса валюты для заданной валюты.
+  /// - Parameter currency: Валюта.
+  /// - Returns: Кортеж с моделями виджета и курса валюты или `nil`, если не найдено.
+  func getCurrencyModels(for currency: CurrencyRate.Currency) -> (WidgetCryptoView.Model, CurrencyRateIdentifiable)? {
+    guard let currencyWidgetsModel = currencyWidgets.first(where: { $0.additionalID == currency.rawValue }),
+          let currencyRateIdentifiableModel = currencyRateIdentifiable.first(where: { $0.currency.rawValue == currency.rawValue }) else {
+      return nil
+    }
+    return (currencyWidgetsModel, currencyRateIdentifiableModel)
+  }
+  
+  /// Обновляет модель виджета валюты данными из идентифицируемой модели курса.
+  /// - Parameters:
+  ///   - currencyWidgetsModel: Модель виджета валюты.
+  ///   - currencyRateIdentifiableModel: Модель идентифицируемого курса валюты.
+  func updateCurrencyWidgetsModel(
+    _ currencyWidgetsModel: WidgetCryptoView.Model,
+    with currencyRateIdentifiableModel: CurrencyRateIdentifiable
+  ) {
+    if let rightSideLargeTextModel = currencyWidgetsModel.rightSideLargeTextModel {
+      rightSideLargeTextModel.text = currencyRateIdentifiableModel.rateText
+      rightSideLargeTextModel.textStyle = currencyRateIdentifiableModel.rateDouble == .zero ? .netural : .standart
+      currencyWidgetsModel.rightSideLargeTextModel = rightSideLargeTextModel
+    }
+    
+    if let keyboardModelUpdate = currencyWidgetsModel.keyboardModel {
+      keyboardModelUpdate.value = currencyRateIdentifiableModel.rateText
       currencyWidgetsModel.keyboardModel = keyboardModelUpdate
     }
   }
   
-  func modifyAmount(_ newValue: String) -> String {
-    let onlyCommaValue = newValue == "," ? "0," : newValue
-    let newValueEmpty = onlyCommaValue.isEmpty ? "0" : onlyCommaValue
-    return newValueEmpty
-  }
-  
-  func setRawCurrencyRate(newValue: String) async {
-    let newValueReplaceDoubleZeroWithZero = interactor.textFormatterService.replaceDoubleZeroWithZero(in: newValue)
-    let newValueRemoveLeadingZeroIfNoComma = interactor.textFormatterService.removeLeadingZeroIfNoComma(
-      in: newValueReplaceDoubleZeroWithZero
-    )
-    
-    let rawClearedTextCurrencyRate = interactor.textFormatterService.removeAllSpaces(from: newValueRemoveLeadingZeroIfNoComma)
-    let rawThousandsCurrencyRate = interactor.textFormatterService.formatNumberWithThousands(rawClearedTextCurrencyRate)
-    rawCurrencyRate = rawThousandsCurrencyRate
-    await saveEnteredCurrencyAmountRaw()
-  }
-  
-  func saveEnteredCurrencyAmountRaw() async {
-    let newValueRemoveAllSpaces = interactor.textFormatterService.removeAllSpaces(from: rawCurrencyRate)
-    let testReplaceCommasWithDots = interactor.textFormatterService.replaceCommasWithDots(in: newValueRemoveAllSpaces)
-    await interactor.setEnteredCurrencyAmountRaw(testReplaceCommasWithDots)
-  }
-  
-  func getEnteredCurrencyAmountRaw(_ appSettingsModel: AppSettingsModel) async -> String {
-    let enteredCurrencyAmountRaw = appSettingsModel.enteredCurrencyAmountRaw
-    let rawReplaceDotsWithCommas = interactor.textFormatterService.replaceDotsWithCommas(in: enteredCurrencyAmountRaw)
-    let rawThousandsCurrencyRate = interactor.textFormatterService.formatNumberWithThousands(rawReplaceDotsWithCommas)
-    return rawThousandsCurrencyRate
+  /// Обновляет модель клавиатуры для заданного виджета валюты.
+  /// - Parameters:
+  ///   - currencyWidgetsModel: Модель виджета валюты.
+  ///   - currencyRateIdentifiableModel: Модель идентифицируемого курса валюты.
+  ///   - isShown: Логическое значение отображения клавиатуры.
+  func updateKeyboardModel(
+    for currencyWidgetsModel: WidgetCryptoView.Model,
+    with currencyRateIdentifiableModel: CurrencyRateIdentifiable,
+    isShown: Bool
+  ) {
+    if let keyboardModelUpdate = currencyWidgetsModel.keyboardModel {
+      keyboardModelUpdate.isKeyboardShown = isShown
+      keyboardModelUpdate.value = currencyRateIdentifiableModel.rateText
+      currencyWidgetsModel.keyboardModel = keyboardModelUpdate
+    }
   }
 }
 
